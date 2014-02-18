684ea3bf11d0157d35ed7a279d697934208612d0
snehaljayade <snehal.jayade@gmail.com>
2014-02-18 09:59:47 +0530

    Added new models.

---
+++ application/models/hotel_reservation.php
@@ -0,0 +1,50 @@
+<?php
+class Hotel_reservation extends CI_Model{
+		var $hotelres_id;
+		var $eventreg_id;
+		var $eventhotel_id;
+		var $roomno;
+		var $checkin;
+		var $checkout;
+		var $comment;
+		
+	   
+			
+		static $tablename = 'hotel_reservation';
+		static $tableid = 'hotelres_id';
+		function find_by_id($id){
+			$tableid = self::$tableid;
+			$resultset = $this->db->get_where(self::$tablename,array($tableid=>$id),1);
+			if($resultset->num_rows()==1)
+				return array_shift($resultset->result());
+			return false;
+		}
+		
+		function find_all(){
+			$resultset = $this->db->get(self::$tablename);
+			return $resultset->result();
+		}
+		
+		function save(){
+		$tableid = self::$tableid;
+			if(isset($this->$tableid)&&$this->$tableid!=''&&$this->$tableid!=0)
+				$this->update();
+			else
+				$this->insert();
+		}
+		private function insert(){
+			$this->db->insert(self::$tablename,$this);
+		}
+		
+		private function update(){
+		$tableid = self::$tableid;
+			$this->db->where($tableid,$this->$tableid);
+			$this->db->update(self::$tablename,$this);
+		}
+		
+		function delete(){
+		$tableid = self::$tableid;
+			$this->db->where($tableid,$this->$tableid);
+			$this->db->delete(self::$tablename);
+		}
+	}
---
+++ application/models/other_qualification_detail.php
@@ -0,0 +1,47 @@
+<?php
+class Other_qualification_detail extends CI_Model{
+		var $oq_id;
+		var $alumni_id;
+		var $name_of_institute;
+	    var $course;
+		var $year_of_passing;
+		var $extras;
+			
+		static $tablename = 'other_qualification_detail';
+		static $tableid = 'oq_id';
+		function find_by_id($id){
+			$tableid = self::$tableid;
+			$resultset = $this->db->get_where(self::$tablename,array($tableid=>$id),1);
+			if($resultset->num_rows()==1)
+				return array_shift($resultset->result());
+			return false;
+		}
+		
+		function find_all(){
+			$resultset = $this->db->get(self::$tablename);
+			return $resultset->result();
+		}
+		
+		function save(){
+		$tableid = self::$tableid;
+			if(isset($this->$tableid)&&$this->$tableid!=''&&$this->$tableid!=0)
+				$this->update();
+			else
+				$this->insert();
+		}
+		private function insert(){
+			$this->db->insert(self::$tablename,$this);
+		}
+		
+		private function update(){
+		$tableid = self::$tableid;
+			$this->db->where($tableid,$this->$tableid);
+			$this->db->update(self::$tablename,$this);
+		}
+		
+		function delete(){
+		$tableid = self::$tableid;
+			$this->db->where($tableid,$this->$tableid);
+			$this->db->delete(self::$tablename);
+		}
+	}
